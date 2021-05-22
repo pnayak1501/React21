f3c409c2e00de38f07a2ae8c6de36973c0fe62f5
pnayak1501 <pnayak1501@gmail.com>
2021-05-22 17:01:42 +0530

    commit 8-8

--- 08context-reducer-todo/src/Components/TodoForm.js
+++ 08context-reducer-todo/src/Components/TodoForm.js
@@ -5,8 +5,27 @@ import {TodoContext} from "../context/TodoContext"
 import {ADD_TODO} from "../context/action.types"
 
 const TodoForm = () =>{
+    const [todoString, setTodoString] = useState("");
+    const {dispatch} = useContext(TodoContext);
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        if(todoString == ""){
+            return alert("Please enter a Todo");
+        }
+        const todo = {
+            todoString,
+            id: v4()
+        }
+        dispatch({
+            type: ADD_TODO,
+            payload: todo
+        })
+        setTodoString("")
+    }
+
     return(
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup>
                 <InputGroup>
                     <Input 
@@ -14,6 +33,8 @@ const TodoForm = () =>{
                         name = "todo"
                         id = "todo"
                         placeholder = "Your next Todo"
+                        value={todoString}
+                        onChange={e => setTodoString(e.target.value)}
                     />
                     <InputGroupAddon addonType="prepend">
                         <Button color="success">
