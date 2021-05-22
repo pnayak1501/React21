a256c803b52bd98f727f6d39eb509184b8192e24
pnayak1501 <pnayak1501@gmail.com>
2021-05-22 16:53:54 +0530

    commit 8-7

---
+++ 08context-reducer-todo/src/Components/TodoForm.js
@@ -0,0 +1,29 @@
+import React, {useState, useContext} from "react"
+import {FormGroup, Input, Button, Form, InputGroup, InputGroupAddon} from "reactstrap"
+import {v4} from "uuid"
+import {TodoContext} from "../context/TodoContext"
+import {ADD_TODO} from "../context/action.types"
+
+const TodoForm = () =>{
+    return(
+        <Form>
+            <FormGroup>
+                <InputGroup>
+                    <Input 
+                        type = "text"
+                        name = "todo"
+                        id = "todo"
+                        placeholder = "Your next Todo"
+                    />
+                    <InputGroupAddon addonType="prepend">
+                        <Button color="success">
+                            Add
+                        </Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    )
+}
+
+export default TodoForm;
