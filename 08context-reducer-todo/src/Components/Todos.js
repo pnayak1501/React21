cdc83ba0dad6698d4140a62f7a2b0c63166925c4
pnayak1501 <pnayak1501@gmail.com>
2021-05-22 17:12:54 +0530

    commit 8-10

--- 08context-reducer-todo/src/Components/Todos.js
+++ 08context-reducer-todo/src/Components/Todos.js
@@ -6,6 +6,19 @@ import {REMOVE_TODO} from "../context/action.types"
 
 
 const Todos = () =>{
+    const {todos, dispatch} = useContext(TodoContext);
+
+    return(
+        <ListGroup className="mt-5 mb-2 items">
+            {todos.map(todo => {
+                return(
+                    <ListGroupItem key={todo.id}>
+                        {todo.todoString}
+                    </ListGroupItem>
+                )
+            })}
+        </ListGroup>
+    )
 
 }
 
