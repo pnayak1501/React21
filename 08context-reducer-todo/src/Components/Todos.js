453ab1e112b4680120e23beb4b5f4364646b1a71
pnayak1501 <pnayak1501@gmail.com>
2021-05-22 17:07:12 +0530

    commit 8-9

---
+++ 08context-reducer-todo/src/Components/Todos.js
@@ -0,0 +1,12 @@
+import React, {useContext} from "react"
+import {ListGroup, ListGroupItem} from "reactstrap"
+import {FaCheckDouble} from "react-icons/fa"
+import {TodoContext} from "../context/TodoContext"
+import {REMOVE_TODO} from "../context/action.types"
+
+
+const Todos = () =>{
+
+}
+
+export default Todos;
