2f26cd2c2b70034e625e23bb221e2b6782c027c9
pnayak1501 <pnayak1501@gmail.com>
2021-07-01 21:48:14 +0530

    Github

---
+++ 12firebase-github-app/src/Config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyD5UWekanE0VpPzdZ49Hffibg8APpv2T1s",
+  authDomain: "mygitapp-91bd8.firebaseapp.com",
+  projectId: "mygitapp-91bd8",
+  storageBucket: "mygitapp-91bd8.appspot.com",
+  messagingSenderId: "319045382096",
+  appId: "1:319045382096:web:6d5a48c760bb50b9eb13d5",
+  measurementId: "G-RM6Q2FWJ6E"
+};
---
+++ side-two-myrouter/src/Visit.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Visit(){
+  return(
+    <div>
+      <h1> This is my Visit Component</h1>
+    </div>
+  );
+}
